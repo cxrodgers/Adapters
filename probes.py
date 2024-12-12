e57bff36c7cda290b8e01a547710625fc8aadfbb
Chris Rodgers <xrodgers@gmail.com>
2024-12-11 19:40:34 -0500

    remove adrians probe

--- probes.py
+++ probes.py
@@ -3,53 +3,8 @@
 from builtins import range
 import Adapters
 
-## This is for Adrian's 1x8 shank array
-# This one goes from "electrode #" (depth?) to "interposer PCB #"
-adrian_8shank2interposer = Adapters.Adapter(
-    [27, 25, 21, 20, 9],
-    [41, 45, 53, 55, 52],
-)
 
-# This one goes from interposer PCB # to Samtec numbers
-# This uses the little handbuild adapter I made that is an ON1 (I think)
-# superglued to a breadboard PCB and hand-wired.
-# Here I will use 41-80 as Samtec numbers, with 1 in the upper left when
-# looking into the next adapter (out of the probe).
-# The reason to use 41-80 is because this will be used with the bottom
-# connector on ON4.
-interposer2samtec = Adapters.Adapter([
-    33, 34, 35, 36, 37, 38, 39, 40, # first column (shank side to top side)
-    41, 42, 43, 44, 45, 46, 47, 48, # second column (top side to shank side)
-    49, 50, 51, #52, 53, 54, 55, # third column (shank side to top side)
-    ], [
-    53, 57, 58, 61, 62, 65, 73, 77,
-    49, 45, 41, 80, 76, 72, 68, 63, 
-    64, 60, 56,
-    ]
-)
-
-# Hack the above
-# The inner column of interposer doesn't have any useful sites, according
-# to Adrian. And the outer column of my adapter isn't wired up fully.
-# So, shift the interposer such that its inner column is floating.
-# Same as above, except 
-interposer2samtec_shifted = Adapters.Adapter([
-    #33, 34, 35, 36, 37, 38, 39, 40, # first (innermost) column (shank side to top side)
-    48, 47, 46, 45, 44, 43, 42, 41, # second column (shank side to top side)
-    #41, 42, 43, 44, 45, 46, 47, 48, # second column (top side to shank side)
-    49, 50, 51, 52, 53, 54, 55, # third column (shank side to top side)
-    ], [
-    53, 57, 58, 61, 62, 65, 73, 77,
-    49, 45, 41, 80, 76, 72, 68, #63, 
-    #64, 60, 56,
-    ]
-)
-
-
-## End Adrian's array
-
-
-# This is the A32 connector pinout from neuronexus.
+## This is the A32 connector pinout from neuronexus.
 # Takes us from "Samtec numbers" to Neuronexus channel numbers.
 # Samtec numbers go from 1-40, with 1 in the upper right when looking at
 # the probe, or 1 in the upper left when looking at the adapter.
